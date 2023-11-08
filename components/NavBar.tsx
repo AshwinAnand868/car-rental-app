99f7b32f3ef1ba380f2f3db797d4ca62c7caa02f
Ashwin Anand <anandashwin@gmail.com>
2023-11-08 14:23:27 -0500

    Enhance the styling for NavBar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,12 +4,18 @@ import { UserButton } from '@clerk/nextjs'
 
 function NavBar() {
     return (
-        <div>
+        <div className="flex items-center justify-between p-5 px-5 shadow-sm border-b">
             <Image src='/logo.png' alt="Logo" width={100} height={100}/>
-            <div>
-                <h2>Home</h2>
-                <h2>History</h2>
-                <h2>Contact Us</h2>
+            <div className="hidden md:flex gap-5">
+                <h2 className="hover:bg-blue-500
+                    px-3 cursor-pointer
+                    p-2 rounded-full hover:text-white">Home</h2>
+                <h2 className="hover:bg-blue-500
+                    px-3 cursor-pointer
+                    p-2 rounded-full hover:text-white">History</h2>
+                <h2 className="hover:bg-blue-500
+                    px-3 cursor-pointer
+                    p-2 rounded-full hover:text-white">Contact Us</h2>
             </div>
             <UserButton />
         </div>
