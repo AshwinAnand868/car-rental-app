590dcd5f71af9f3f2d15cc59edbf336bdf00cbfc
Ashwin Anand <anandashwin@gmail.com>
2023-11-10 16:02:28 -0500

    Install Daisy UI Tailwind CSS library

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Raleway } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider, SignedIn, SignedOut, SignIn, UserButton } from '@clerk/nextjs'
 import NavBar from '@/components/NavBar'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Raleway({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import CarsFilterOptions from '@/components/Home/CarsFilterOptions'
+import Hero from '@/components/Home/Hero'
+import SearchInput from '@/components/Home/SearchInput'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-      <h2>Hello, I am Ashwin, and this is my first nextjs app.</h2>
+    <div className="p-5 sm:px-10 md:px-20">
+      <Hero />
+      <SearchInput />
+      <CarsFilterOptions />
     </div>  
   )
 }
