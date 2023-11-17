ee26c544a06fc09b176f76bdec3bf11944aab1f1
AshwinAnand868 <anandashwin868@gmail.com>
2023-11-16 20:54:37 -0500

    Implement filtering based on car brand

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,13 @@ import { getCarsList } from '@/services'
 import CarsListView from '@/components/Home/CarsListView'
 import Car from '@/models/Car'
 import CarsList from '@/models/CarsList';
+import CarBrand from '@/models/CarBrand'
 
 export default function Home() {
 
+  const ALL_AVAILABLE_BRANDS = 'All Available Brands';
   const [carsList, setCarsList] = useState<Array<Car>>([]);
+  const [carsOrgList, setCarsOrgList] = useState<Array<Car>>([]);
 
   useEffect(() => {
     carList();
@@ -19,13 +22,23 @@ export default function Home() {
   const carList = async () => {
     const result = await getCarsList() as CarsList;
     setCarsList(result?.carLists);
+    setCarsOrgList(result?.carLists);
   } 
+
+  const filterCarList = (brand: string) => {
+    if(brand !== ALL_AVAILABLE_BRANDS){
+      setCarsList(carsOrgList.filter((car: Car) => car.carBrand === brand));
+    } else {
+      setCarsList(carsOrgList);
+    }
+  }
 
   return (
     <div className="p-5 sm:px-10 md:px-20">
       <Hero />
       <SearchInput />
-      <CarsFilterOptions />
+      <CarsFilterOptions carsOrgList={carsOrgList} 
+        setBrandType={value => filterCarList(value)}/>
       <CarsListView carLists={carsList}/>
     </div>  
   )
--- components/Home/CarsFilterOptions.tsx
+++ components/Home/CarsFilterOptions.tsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import Car from '@/models/Car'
+import CarBrand from '@/models/CarBrand'
+import CarsList from '@/models/CarsList'
+import React, { useEffect, useState } from 'react'
 
-function CarsFilterOptions() {
+interface CarsOrgListProp {
+  carsOrgList: Array<Car>,
+  setBrandType: (brand: string) => void 
+}
+
+function CarsFilterOptions({carsOrgList, setBrandType}: CarsOrgListProp) {
+
+  const [brandList, setBrandList] = useState<CarBrand[]>();
+  const carBrandSet = new Set();
+  carBrandSet.add('All Available Brands');
+  const filterCarList = () => {
+    carsOrgList.forEach((element: Car) => {
+      carBrandSet.add(element.carBrand); 
+    });
+    setBrandList(Array.from(carBrandSet) as CarBrand[]);
+  }
+
+  useEffect(() => {
+    if(carsOrgList) {
+      filterCarList();
+    }
+  }, [carsOrgList]);
+
   return (
     <div className='flex mt-10 items-center justify-between'>
       <div>
@@ -13,11 +38,12 @@ function CarsFilterOptions() {
             <option>Min to Max</option>
             <option>Max to Min</option>
         </select>
-        <select className="select select-bordered w-full max-w-xs hidden md:block">
+        <select className="select select-bordered w-full max-w-xs hidden md:block"
+        onChange={e => setBrandType(e.target.value)}>
             <option disabled selected>Manufacturer</option>
-            <option>Honda</option>
-            <option>BMW</option>
-            <option>Toyota</option>
+            {brandList?.map((brand: CarBrand, index: number) => (
+              <option key={index}>{brand}</option>
+            ))}
         </select>
       </div>
     </div>
