f7d69657c525680e9e963f5436287c384e373b93
AshwinAnand868 <anandashwin868@gmail.com>
2023-11-18 14:43:46 -0500

    Sorting cars based on price

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import CarsFilterOptions from '@/components/Home/CarsFilterOptions'
 import Hero from '@/components/Home/Hero'
 import SearchInput from '@/components/Home/SearchInput'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { getCarsList } from '@/services'
 import CarsListView from '@/components/Home/CarsListView'
 import Car from '@/models/Car'
@@ -14,10 +14,14 @@ export default function Home() {
   const ALL_AVAILABLE_BRANDS = 'All Available Brands';
   const [carsList, setCarsList] = useState<Array<Car>>([]);
   const [carsOrgList, setCarsOrgList] = useState<Array<Car>>([]);
+  const sortingOrder = useRef('');
+  const selectedBrand = useRef('');
 
   useEffect(() => {
     carList();
   }, []);
+
+
 
   const carList = async () => {
     const result = await getCarsList() as CarsList;
@@ -26,11 +30,38 @@ export default function Home() {
   } 
 
   const filterCarList = (brand: string) => {
+    selectedBrand.current = brand;
+    let filteredOutCars = [];
     if(brand !== ALL_AVAILABLE_BRANDS){
-      setCarsList(carsOrgList.filter((car: Car) => car.carBrand === brand));
+      filteredOutCars = carsOrgList.filter((car: Car) => car.carBrand === brand);
     } else {
-      setCarsList(carsOrgList);
+      filteredOutCars = [...carsOrgList];
     }
+
+    if(sortingOrder.current !== '') {
+      filteredOutCars.sort((carA: Car, carB: Car) => 
+        sortingOrder.current === 'ascending' ? 
+          carA.price - carB.price
+          : carB.price - carA.price
+      );
+    }
+    setCarsList(filteredOutCars);
+  }    
+  
+  const sortCarsByPrice = (orderingCriteria: string) => {
+    sortingOrder.current = orderingCriteria;
+
+    let orgListCopy = [...carsOrgList];
+    if(selectedBrand.current !== '' && selectedBrand.current !== ALL_AVAILABLE_BRANDS) {
+      orgListCopy = orgListCopy.filter((car: Car) => car.carBrand === selectedBrand.current);
+    }
+
+    const sortedCars = orgListCopy.sort((carA: Car, carB: Car) => 
+      orderingCriteria === 'ascending' ? 
+        carA.price - carB.price 
+        : carB.price - carA.price
+    );
+    setCarsList(sortedCars);
   }
 
   return (
@@ -38,7 +69,9 @@ export default function Home() {
       <Hero />
       <SearchInput />
       <CarsFilterOptions carsOrgList={carsOrgList} 
-        setBrandType={value => filterCarList(value)}/>
+        setBrandType={(value: string) => filterCarList(value)}
+        sortCarsByPrice={(orderingCriteria: string) => sortCarsByPrice(orderingCriteria)}
+      />
       <CarsListView carLists={carsList}/>
     </div>  
   )
--- components/Home/CarsFilterOptions.tsx
+++ components/Home/CarsFilterOptions.tsx
@@ -1,14 +1,15 @@
 import Car from '@/models/Car'
 import CarBrand from '@/models/CarBrand'
 import CarsList from '@/models/CarsList'
-import React, { useEffect, useState } from 'react'
+import React, { ChangeEvent, useEffect, useState } from 'react'
 
 interface CarsOrgListProp {
   carsOrgList: Array<Car>,
-  setBrandType: (brand: string) => void 
+  setBrandType: (brand: string) => void,
+  sortCarsByPrice: (orderingCriteria: string) => void
 }
 
-function CarsFilterOptions({carsOrgList, setBrandType}: CarsOrgListProp) {
+function CarsFilterOptions({carsOrgList, setBrandType, sortCarsByPrice}: CarsOrgListProp) {
 
   const [brandList, setBrandList] = useState<CarBrand[]>();
   const carBrandSet = new Set();
@@ -33,14 +34,14 @@ function CarsFilterOptions({carsOrgList, setBrandType}: CarsOrgListProp) {
         <h2>Explore our cars you might like</h2>
       </div>
       <div className='flex gap-5'>
-        <select className="select select-bordered w-full max-w-xs">
+        <select className="select select-bordered w-full max-w-xs"
+          onChange={(e: React.ChangeEvent<HTMLSelectElement>) => sortCarsByPrice(e.target.value)}>
             <option disabled selected>Price</option>
-            <option>Min to Max</option>
-            <option>Max to Min</option>
+            <option value={'ascending'}>Min to Max</option>
+            <option value={'descending'}>Max to Min</option>
         </select>
         <select className="select select-bordered w-full max-w-xs hidden md:block"
-        onChange={e => setBrandType(e.target.value)}>
-            <option disabled selected>Manufacturer</option>
+        onChange={(e: React.ChangeEvent<HTMLSelectElement>) => setBrandType(e.target.value)}>            <option disabled selected>Manufacturer</option>
             {brandList?.map((brand: CarBrand, index: number) => (
               <option key={index}>{brand}</option>
             ))}
