22f3ead7d546a702b7067533e592ab6400c289a9
AshwinAnand868 <anandashwin868@gmail.com>
2023-12-17 09:03:28 -0500

    Add search bar and divide the store locator Modal into two parts

--- app/Home/SearchInput.tsx
+++ app/Home/SearchInput.tsx
@@ -2,6 +2,7 @@ import { Popup } from 'semantic-ui-react';
 import 'semantic-ui-css/components/popup.min.css';
 import { useState } from 'react';
 import Modal from '../Modal/Modal';
+import SearchBar from '../SearchBar/SearchBar';
 
 function SearchInput() {
 
@@ -49,11 +50,20 @@ function SearchInput() {
                     onDismiss={closeModalHandler}
                     title={'Select your preferred store location...'}
                 >
-                    <div className='my-4 w-[800px] max-w-full h-[600px]'>
+                    {/* <div className='my-4 max-w-full'>
                         <p>This is where you will find a search bar to search and select our different store locations and also a map on the right hand side to see and select your preferred store location directly!</p>
-                    </div>
-                </Modal>
+                    </div> */}
 
+                    <div className='grid grid-cols-1 md:grid-cols-2 px-6 md:px-10 max-w-full mt-5 md:w-[800px] h-[650px] md:[h-550px]'>
+                        <div>
+                            <SearchBar />
+                        </div>
+                        <div>
+                            Google Map
+                        </div>
+                    </div>
+
+                </Modal>
             </div>
         </div>
     )
---
+++ app/SearchBar/SearchBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SearchBar = () => {
+    return (
+        <div className='flex gap-3 bg-gray-100 text-gray-400 p-2 rounded-xl'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-black">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+            </svg>
+            <input type='text' placeholder='Search Store' className='bg-transparent outline-none w-full text-[17px]'/>
+        </div>
+    )
+}
+
+export default SearchBar;
