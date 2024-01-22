e905fae85555cabfb839ea5a88e04e033660456b
AshwinAnand868 <anandashwin868@gmail.com>
2024-01-22 12:19:55 -0500

    Add simple skeleton for Social Media Icons

---
+++ app/Footer/SocialMediaIcons.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { FaFacebook } from "react-icons/fa";
+
+const SocialMediaIcons = () => {
+  return (
+    <div>
+        <ul className='list-none'>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>l
+    </div>
+  )
+}
+
+export default SocialMediaIcons
