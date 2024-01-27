09ef9155a00c63dd9d7daa24e8343d0e85c08db9
AshwinAnand868 <anandashwin868@gmail.com>
2024-01-27 13:36:19 -0500

    Add social media icons to footer

--- app/Footer/Newsletter.tsx
+++ app/Footer/Newsletter.tsx
@@ -23,9 +23,9 @@ const Newsletter = () => {
 
     return (
         <>
-            <h1 className='text-[16px] md:text-[20px] mb-2 font-bold'>Newsletter</h1>
-            <p className="text-[12px] md:text-[16px]">Don't miss a thing! Sign up to receive daily deals</p>
-            <form className="flex flex-col justify-center w-[43%] md:w-[85%] min-w-[182px] mt-4 text-[12px] md:text-[16px]" onSubmit={handleSubmit}>
+            <h1 className='text-2xl'>Newsletter</h1>
+            <p className="text-lg text-gray-400 mt-2">Don't miss a thing! Sign up to receive daily deals</p>
+            <form className="flex flex-col justify-center mt-4 text-[12px] md:text-[16px]" onSubmit={handleSubmit}>
                 <input aria-label="Your email address" type="email" className="input input-bordered outline-none text-gray-600"
                     value={email} placeholder="Your Email Address" onChange={handleChange}></input>
                 <button type='submit' className="btn mt-4 bg-gray-100 rounded-md text-gray-600">Subscribe</button>
--- app/Footer/SocialMediaIcons.tsx
+++ app/Footer/SocialMediaIcons.tsx
@@ -1,17 +1,60 @@
+import Link from 'next/link';
 import React from 'react'
-import { FaFacebook } from "react-icons/fa";
+import { FaFacebookF, FaTwitter, FaLinkedinIn, FaInstagram } from "react-icons/fa";
+import { FiGithub } from "react-icons/fi";
 
 const SocialMediaIcons = () => {
+
+  const linkStyles = {
+    backgroundColor: "#f5f6fa",
+    display: "block",
+    height: "53px",
+    width: "53px",
+    borderRadius: "53px",
+  }
+
+  const socialLinks = [
+    {
+      id: 1,
+      icon: FaFacebookF,
+      url: "https://www.facebook.com/anand.ashwin.9212/",
+    },
+    {
+      id: 2,
+      icon: FaTwitter,
+      url: "https://twitter.com/_Ashwin_Anand_",
+    },
+    {
+      id: 3,
+      icon: FaLinkedinIn,
+      url: "https://www.linkedin.com/in/anand-ashwin/",
+    },
+    {
+      id: 4,
+      icon: FaInstagram,
+      url: "https://www.instagram.com/ashwin_anand_/",
+    },
+    {
+      id: 5,
+      icon: FiGithub,
+      url: "https://github.com/AshwinAnand868/",
+    },
+  ];
+
   return (
-    <div>
-        <ul className='list-none'>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>l
-    </div>
+    <>
+      {socialLinks.map((socialLink, index) => {
+        const Icon = socialLink.icon;
+        return (
+          <Link
+            key={`social-${index}`} target='_blank' href={socialLink.url}
+            className='mt-3 p-3 rounded-full bg-blue-500 hover:bg-white hover:text-blue-500 cursor-pointer'
+            >
+            <Icon className='w-5 h-5'/>
+          </Link>
+        );
+      })}
+    </>
   )
 }
 
