cab6fa922512053f00b328cdd76178bf957758e5
AshwinAnand868 <anandashwin868@gmail.com>
2024-02-24 13:05:37 -0500

    Update Testimonials Styles

--- app/Testimonials/Testimonials.tsx
+++ app/Testimonials/Testimonials.tsx
@@ -1,65 +1,68 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import { Container } from 'react-bootstrap';
 import Carousel from 'react-bootstrap/Carousel';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-// import ExampleCarouselImage from 'components/ExampleCarouselImage';
-
-// import { MDBCarousel, MDBContainer, MDBCarouselItem, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit'
-// import Image from 'next/image'
-// import React from 'react'
-// import { FaQuoteLeft } from 'react-icons/fa'
+import './Testimonials.css';
 
 const Testimonials = () => {
 
+    const [index, setIndex] = useState<number>(0)
+
+    const handleSelect = (selectedIndex: number) => {
+        console.log("Selected Index" + selectedIndex);
+        setIndex(selectedIndex);
+    }
+
     return (
-        <div className='tw-flex tw-justify-center'>
-            <Container className='tw-my-10 tw-flex tw-flex-col tw-justify-center tw-items-center' fluid>
-                <Carousel>
-                    <Carousel.Item>
-                        {/* <ExampleCarouselImage text="First slide" /> */}
+        <div className='tw-flex tw-justify-center tw-h-[400px] tw-my-10'>
+            <Container className='tw-flex tw-flex-col tw-justify-center tw-items-center !tw-text-black' fluid>
+                <span className='tw-text-[3rem] tw-font-semibold tw-italic'>What Our Clients Have to Say</span>
+                <Carousel className='tw-w-[100%] tw-h-[80%]' activeIndex={index} onSelect={handleSelect}>
+                    <Carousel.Item className='tw-flex tw-justify-center tw-items-center'>
                         <Image
                             src='https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(10).webp'
                             alt='Customer Image'
-                            width={120}
-                            height={120}
+                            width={200}
+                            height={200}
                             className='tw-rounded-[50%] tw-mb-4'
                         />
                         <Carousel.Caption>
-                            <h3>First slide label</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            <blockquote className="tw-p-4 tw-my-4 tw-border-s-4 tw-border-gray-300 tw-bg-gray-50 dark:tw-border-gray-500 dark:tw-bg-gray-800">
+                                <h4>First slide label</h4>
+                                <p className="tw-text-xl tw-italic tw-font-medium tw-leading-relaxed tw-text-gray-900 dark:tw-text-white">"Flowbite is just awesome. It contains tons of predesigned components and pages starting from login screen to complex dashboard. Perfect choice for your next SaaS application."</p>
+                            </blockquote>
                         </Carousel.Caption>
                     </Carousel.Item>
-                    <Carousel.Item>
+                    <Carousel.Item className='tw-flex tw-justify-center tw-items-center'>
                         <Image
                             src='https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(10).webp'
                             alt='Customer Image'
-                            width={120}
-                            height={120}
+                            width={200}
+                            height={200}
                             className='tw-rounded-[50%] tw-mb-4'
                         />
-                        {/* <ExampleCarouselImage text="Second slide" /> */}
                         <Carousel.Caption>
-                            <h3>Second slide label</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            <blockquote className="tw-p-4 tw-my-4 tw-border-s-4 tw-border-gray-300 tw-bg-gray-50 dark:tw-border-gray-500 dark:tw-bg-gray-800">
+                                <h4>Second slide label</h4>
+                                <p className="tw-text-xl tw-italic tw-font-medium tw-leading-relaxed tw-text-gray-900 dark:tw-text-white">"Flowbite is just awesome. It contains tons of predesigned components and pages starting from login screen to complex dashboard. Perfect choice for your next SaaS application."</p>
+                            </blockquote>
                         </Carousel.Caption>
                     </Carousel.Item>
-                    <Carousel.Item>
+                    <Carousel.Item className='tw-flex tw-justify-center tw-items-center'>
                         <Image
                             src='https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(10).webp'
                             alt='Customer Image'
-                            width={120}
-                            height={120}
+                            width={200}
+                            height={200}
                             className='tw-rounded-[50%] tw-mb-4'
                         />
-                        {/* <ExampleCarouselImage text="Third slide" /> */}
                         <Carousel.Caption>
-                            <h3>Third slide label</h3>
-                            <p>
-                                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                            </p>
+                            <blockquote className="tw-p-4 tw-my-4 tw-border-s-4 tw-border-gray-300 tw-bg-gray-50 dark:tw-border-gray-500 dark:tw-bg-gray-800">
+                                <h4>Third slide label</h4>
+                                <p className="tw-text-xl tw-italic tw-font-medium tw-leading-relaxed tw-text-gray-900 dark:tw-text-white">"Flowbite is just awesome. It contains tons of predesigned components and pages starting from login screen to complex dashboard. Perfect choice for your next SaaS application."</p>
+                            </blockquote>
                         </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel>
@@ -67,51 +70,6 @@ const Testimonials = () => {
         </div>
 
     );
-    //     return (
-    //         <div className='flex justify-center'>
-    //             <MDBContainer className='my-10 flex flex-col justify-center items-center w-[50%]'>
-    //                 <MDBCarousel showControls showIndicators>
-    //                     <MDBCarouselItem itemId={1} className="active text-center flex items-center flex-col">
-    //                         <Image
-    //                             src='https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(10).webp'
-    //                             alt='Customer Image'
-    //                             width={120}
-    //                             height={120}
-    //                             className='rounded-[50%] shadow-2xl mb-4'
-    //                         />
-    //                         <MDBRow>
-    //                             <MDBCol>
-    //                                 <h5 className='mb-3'>Maria Kate</h5>
-    //                                 <p className="text-gray-500">
-    //                                     <FaQuoteLeft className="text-[20px]"/>
-    //                                     <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
-    //                                     et deleniti nesciunt sint eligendi reprehenderit reiciendis,
-    //                                     quibusdam illo, beatae quia fugit consequatur laudantium velit
-    //                                     magnam error. Consectetur distinctio fugit doloremque.</span>
-    //                                 </p>
-    //                             </MDBCol>
-    //                         </MDBRow>
-    //                     </MDBCarouselItem>
-    //                 </MDBCarousel>
-    //             </MDBContainer>
-    //         </div>
-
-    //         // <div className='mt-10 mb-10'>
-    //         //     <Image
-    //         //         src='/logo.png'
-    //         //         alt='Customer Image'
-    //         //         width={120}
-    //         //         height={120}
-    //         //         className=''
-    //         //     />
-    //         //     Testimonials
-    //         // </div>
-    //         // time kitna bi - portfolio is very important
-    //         // shivji kaho, udemy
-    //         // ancient healings of shiva
-    //         // ohmnamha shivaye
-    //         // track, difference between today and, 
-    //     )
 }
 
 export default Testimonials;
